4abe6e4eacc57e97e3bc6702ef1f0919d1dc40d3
Emin DENİZ <emndeniz@gmail.com>
2020-06-24 22:40:00 +0300

    Film response model added

---
+++ FilmMuzesi/Controller/FilmListesi/FilmListesiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilmListesiTableViewCell.swift
+//  FilmMuzesi
+//
+//  Created by Emin on 24.06.2020.
+//  Copyright © 2020 Baris Gorgun. All rights reserved.
+//
+
+import UIKit
+
+class FilmListesiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FilmMuzesi/Model/FilmResponseModel.swift
@@ -0,0 +1,9 @@
+//
+//  FilmResponseModel.swift
+//  FilmMuzesi
+//
+//  Created by Emin on 24.06.2020.
+//  Copyright © 2020 Baris Gorgun. All rights reserved.
+//
+
+import Foundation
